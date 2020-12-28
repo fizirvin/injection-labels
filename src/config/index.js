9f8194d4be6cdd68ec5e332303397326781bcd24
Irvin Fiz <irvin.fiz.dev@gmail.com>
2020-12-28 11:11:55 -0600

    fix url

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,22 @@
-const url = 'https://injection-labels-server.adrian-injection.vercel.app/graph';
+const url = "https://injection-labels-server.adrian-injection.vercel.app/graph";
 // const url = 'http://localhost:4000/graph';
+// const url = "https://injection-labels-server.irvinfiz.now.sh/graph"
+
 const opts = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" }
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
 };
 
-const hr_server = 'https://hr-app-server.adrian-injection.vercel.app/graph';
+// const hr_server = 'https://hr-app-server.adrian-injection.vercel.app/graph';
 // const hr_server = 'http://localhost:4000/graph';
+// const hr_server = 'https://hr-app-server.irvinfiz.now.sh/graph'
+
+// const hr_server = "http://localhost:4000/injection";
+const hr_server = "https://production-server.vercel.app/injection";
+
 const hr_opts = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" }
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
 };
 
-export { url, opts, hr_server, hr_opts }
+export { url, opts, hr_server, hr_opts };
